76584e8776bbc950da95ba01da6d9249a28cef4c
NicoMN <nicolasmendes.19@hotmail.com>
2019-12-03 15:29:16 -0300

    Added app.js, first tests

--- src/server.js
+++ src/server.js
@@ -1,1 +1,10 @@
-//aaaa
+const http = require('http');
+const app = require('./app');
+
+const host = process.env.HOST || 'localhost';
+const port = process.env.PORT || 8081;
+
+http.createServer(app)
+  .listen(port, () => {
+    console.log(`Server up on http://${host}:${port}`);
+  })
